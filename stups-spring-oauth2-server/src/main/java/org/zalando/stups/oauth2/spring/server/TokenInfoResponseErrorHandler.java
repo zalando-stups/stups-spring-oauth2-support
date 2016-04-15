7b8ab48103d006a9b8562161694f1334256dd92a
Victor Roldan Betancort <victor.roldan.betancort@zalando.de>
2016-04-15 16:18:55 +0200

    Issue #34: TokenInfoResponseErrorHandler does not handle 401 nor 403.

--- stups-spring-oauth2-server/src/main/java/org/zalando/stups/oauth2/spring/server/TokenInfoResponseErrorHandler.java
+++ stups-spring-oauth2-server/src/main/java/org/zalando/stups/oauth2/spring/server/TokenInfoResponseErrorHandler.java
@@ -1,16 +1,14 @@
 package org.zalando.stups.oauth2.spring.server;
 
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
-
-import java.io.IOException;
-
 import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.web.client.DefaultResponseErrorHandler;
+
+import java.io.IOException;
 
 class TokenInfoResponseErrorHandler extends DefaultResponseErrorHandler {
     @Override
     public void handleError(ClientHttpResponse response) throws IOException {
-        if (response.getStatusCode() != BAD_REQUEST) {
+        if (!response.getStatusCode().is4xxClientError()) {
             super.handleError(response);
         }
     }
