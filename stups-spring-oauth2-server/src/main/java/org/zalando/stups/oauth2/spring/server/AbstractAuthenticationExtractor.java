b918b88741ecac755b30f00ee44dd65c590f9639
Joerg Bellmann <joerg.bellmann@zalando.de>
2016-11-28 14:49:16 +0100

    enable license-check

--- stups-spring-oauth2-server/src/main/java/org/zalando/stups/oauth2/spring/server/AbstractAuthenticationExtractor.java
+++ stups-spring-oauth2-server/src/main/java/org/zalando/stups/oauth2/spring/server/AbstractAuthenticationExtractor.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2015 Zalando SE (http://tech.zalando.com)
+ * Copyright (C) 2016 Zalando SE (http://tech.zalando.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
